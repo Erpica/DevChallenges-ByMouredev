2ae1b1cdf6d95a7a2a2d0342747fd479aed978dc
ErPica <erpica@gmail.com>
2026-08-10 14:11:36 +0200

    Finalizado el gituh_cli

--- Python/src/Ejercicios_avanzados/43-github_cli.py
+++ Python/src/Ejercicios_avanzados/43-github_cli.py
@@ -21,51 +21,59 @@ El programa debe permitir las siguientes opciones:
 
 Notas: Todo irá dentro de github_folder
 '''
-# Otra forma menos moderna de llegar al path:
-# import os
-# print(f"{'os.getcwd():':<25} {os.getcwd()}")
+
 
 from pathlib import Path
 import subprocess
+import os
 
-# Directorio padre del archivo con el que estoy trabajando:
-#print(f"{'Path(__file__).parent:':<25} {Path(__file__).parent}")
 current_directory = Path(__file__).parent
 working_directory = current_directory / "github_folder"
-#print(f"Ruta: {working_directory}")
+os.chdir(working_directory)
 
-# Comprueba si existe.
-#print(working_directory.exists())
-
-# Comprueba si es un archivo.
-#print(working_directory.is_file())
-
-# Comprueba si es una carpeta.
-#print(working_directory.is_dir())
 
 def establish_proyect_folder():
+    '''
+    Comprueba si la carpta está creada (siempre dentro de "github_folder").
+
+    Si está creada va a la carpeta superior a esa carpeta y, si no , 
+    la crea y entra en la carpeta superior a ella.
+    '''
     #proyect_folder = input("Introduce el directorio de trabajo de tu proyecto: ")
-    proyect_folder = "Prueba2"
+    proyect_folder = "prueba" # Mientras programo evito el input
     new_folder = working_directory / proyect_folder # creo el objeto en memoria de Python
     if new_folder.exists():
-        pass
+        print("-----------")
+        print(f"\nEl directorio {new_folder} ya existe.")
     else:
+        #print(f"Path: {Path.cwd()}")
         new_folder.mkdir()         # Le digo a Windows que cree la carpeta
         print(f"Has creado correctamente el directorio de trabajo: {proyect_folder}")
+        os.chdir(new_folder.parent)
+        #print(f"Path: {Path.cwd()}")
+        #execute_command(["cd", proyect_folder])
     return new_folder
 
-def execute_command(command): # Lo probaré para iniciar repositorio pero luego será solo para ejecutar comandos.
+def execute_command(command):
+    '''
+    Recibe un comando como un str o como una lista si consta de varias palabras.
+    
+    Devuelve solo la salida. 
+    '''
     #current_repository = input("Introduce el nombre del repositorio de tu proyecto: ")
-    current_repository = "my_repository"
-    result = subprocess.run(
-        command,
-        shell=True,
-        capture_output=True,
-        text=True
-    )
-    print("Salida: ", result.stdout)
-    print("Error: ", result.stderr)
-    print("Código: ", result.returncode)
+    current_repository = "my_repository" # De momento evito el input
+    try:
+        result = subprocess.run(
+            command,
+            shell=True,
+            check=True,
+            capture_output=True,
+            text=True
+        )
+        print(result.stdout.split())
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e.stderr}")
+
 
 while True:
     print('''
@@ -92,33 +100,77 @@ while True:
     except NameError:
         print(f"Estás en el directorio de trabajo: {working_directory}\n")
 
-    option = input("Introcuce una opción: ")
+    option = input("Introduce una opción: ")
 
     match option:
         case "1":
+            #print("Has elegido la opción 1")
             proyect_folder = establish_proyect_folder()
+            
         case "2":
-            execute_command("git init") # solo para probar
+            print("\n\n")
+            #current_repository = input("introduce el nombre del nuevo repositorio")
+            current_repository = "prueba"
+            if Path.exists("prueba"):
+                pass
+            else:
+                print(f"Se ha creado la carpeta y el repositorio {current_repository}.")
+            print("-----------")
+            execute_command(f"git init {current_repository}")
+            
         case "3":
-            pass
+            print("-----------")
+            #new_branch = input("Introduce el nombre de la rama")
+            new_branch = "feature1"
+            execute_command(f"git branch {new_branch}")
+
         case "4":
-            pass
+            print("-----------")
+            new_branch = "feature1"
+            execute_command(f"git switch {new_branch}")
+            
         case "5":
-            pass
+            print("-----------")
+            execute_command("git status")
+            
         case "6":
-            pass
+            print("-----------")
+            execute_command("git add .")
+            print("Guardando ficheros...\n")
+            #message = input("Introduce el mensage para el commit")
+            message = "Mensaje de prueba"
+            execute_command(f'git commit -m "{message}"')
+            
+
         case "7":
-            pass
+            print("-----------")
+            execute_command("git log --online")
+            
         case "8":
-            pass
+            print("-----------")
+            #delete_branch = input("Introduce el nombre de la rama a eliminar: ")
+            delete_branch = "feature1"
+            execute_command(f"git branch -d {delete_branch}")
         case "9":
-            pass
+            print("-----------")
+            #url_remote = input("Introduce la URL remota: ")
+            remote_url = "https://example.com"
+            execute_command(f"git remote remove origin")
+            execute_command(f"git remote add origin {remote_url}")
+            execute_command(f"git push -u origin main")
+
         case "10":
-            pass
+            print("-----------")
+            execute_command("git pull")
+
         case "11":
-            pass
+            print("-----------")
+            execute_command("git push")
+            
         case "12":
             break
+        case _:
+            print("Introduce una opción correcta.")
 
 
 
@@ -127,3 +179,24 @@ while True:
 
 print("\n\n")
 
+
+
+
+'''
+Notas:
+# Otra forma menos moderna de llegar al path:
+# import os
+# print(f"{'os.getcwd():':<25} {os.getcwd()}")
+
+# Comprueba si existe.
+#print(working_directory.exists())
+
+# Comprueba si es un archivo.
+#print(working_directory.is_file())
+
+# Comprueba si es una carpeta.
+#print(working_directory.is_dir())
+
+# Directorio padre del directorio en el que estoy trabajando:
+#print(f"{'Path(__file__).parent:':<25} {Path(__file__).parent}")
+'''
